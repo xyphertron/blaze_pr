aa6ac36657b66e828ef5d42ec135169c55608725
xyphertron <sarveshlan007@gmail.com>
2025-01-13 01:00:08 +0530

    Initial Commmit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.GitStatusBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.GitAddBtn = new System.Windows.Forms.Button();
+            this.LogBoxWindow = new System.Windows.Forms.WebBrowser();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1103, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Open Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(30, 88);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1166, 222);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1069, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Get This Filename";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Get New Timestamp";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Select Category",
+            "SR",
+            "NPL",
+            "FH"});
+            this.comboBox1.Location = new System.Drawing.Point(201, 370);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(319, 371);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(467, 370);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(533, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1069, 368);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Generate Filename";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // GitStatusBtn
+            // 
+            this.GitStatusBtn.Location = new System.Drawing.Point(30, 410);
+            this.GitStatusBtn.Name = "GitStatusBtn";
+            this.GitStatusBtn.Size = new System.Drawing.Size(116, 23);
+            this.GitStatusBtn.TabIndex = 12;
+            this.GitStatusBtn.Text = "Git Status";
+            this.GitStatusBtn.UseVisualStyleBackColor = true;
+            this.GitStatusBtn.Click += new System.EventHandler(this.GitStatusBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(464, 460);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Commit Message";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1015, 368);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(28, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "🗘";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Branch Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(464, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "File Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(467, 488);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(533, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // GitAddBtn
+            // 
+            this.GitAddBtn.Location = new System.Drawing.Point(1069, 405);
+            this.GitAddBtn.Name = "GitAddBtn";
+            this.GitAddBtn.Size = new System.Drawing.Size(115, 23);
+            this.GitAddBtn.TabIndex = 19;
+            this.GitAddBtn.Text = "Switch Branch";
+            this.GitAddBtn.UseVisualStyleBackColor = true;
+            this.GitAddBtn.Click += new System.EventHandler(this.GitAddBtn_Click);
+            // 
+            // LogBoxWindow
+            // 
+            this.LogBoxWindow.Location = new System.Drawing.Point(30, 533);
+            this.LogBoxWindow.MinimumSize = new System.Drawing.Size(20, 20);
+            this.LogBoxWindow.Name = "LogBoxWindow";
+            this.LogBoxWindow.Size = new System.Drawing.Size(970, 199);
+            this.LogBoxWindow.TabIndex = 20;
+            this.LogBoxWindow.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.LogBoxWindow_DocumentCompleted);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1069, 709);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(115, 23);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "Git Push";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 505);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Logs";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(1103, 486);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(81, 23);
+            this.button11.TabIndex = 23;
+            this.button11.Text = "Re-Ammend";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Path:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(147, 53);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(789, 20);
+            this.textBox5.TabIndex = 25;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(198, 416);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Current Directory:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(293, 416);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Please Select Path";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(293, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "?";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(198, 441);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 13);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Current Branch   :";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(1038, 486);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(59, 23);
+            this.button9.TabIndex = 31;
+            this.button9.Text = "Commit";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 773);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.LogBoxWindow);
+            this.Controls.Add(this.GitAddBtn);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GitStatusBtn);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button GitStatusBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button GitAddBtn;
+        private System.Windows.Forms.WebBrowser LogBoxWindow;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string[] ticketCategory { get; set; }
+        public string timestamp;
+        public string selectedFolderPath;
+        private string selectedFileName = string.Empty;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //webBrowser1.DocumentCompleted += webBrowser1_DocumentCompleted;
+           //DisplayContent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GitStatusBtn_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            // Create a ProcessStartInfo object with the command and working directory
+            ProcessStartInfo startInfo = new ProcessStartInfo("git", "status");
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            // Set the working directory to the desired repository path
+            startInfo.WorkingDirectory = selectedFolderPath;
+
+            // Create a new process and start it
+            using (Process process = new Process { StartInfo = startInfo })
+            {
+                process.Start();
+
+                // Read the output from the process
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                // Display the output in the web browser control
+                // Create a temporary HTML file
+                string tempFilePath = Path.GetTempFileName() + ".html";
+
+                // Create the HTML content
+                string htmlContent = $"<pre>{output}</pre>";
+
+                // Write the HTML content to the file
+                File.WriteAllText(tempFilePath, htmlContent);
+
+                // Navigate the WebBrowser to the temporary file
+                LogBoxWindow.Navigate(new Uri(tempFilePath));
+
+
+                process.WaitForExit();
+            }
+        }
+
+        private void LogBoxWindow_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void GitAddBtn_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("git", "add .");
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            // Set the working directory to the desired repository path
+            startInfo.WorkingDirectory = @"F:\git testing pr\testing_blaze_pr";
+
+            // Create a new process and start it
+            using (Process process = new Process { StartInfo = startInfo })
+            {
+                process.Start();
+
+                // Read the output from the process
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                // Display the output in the web browser control
+                // Create a temporary HTML file
+                string tempFilePath = Path.GetTempFileName() + ".html";
+
+                // Create the HTML content
+                string htmlContent = $"<pre>{output}</pre>";
+
+                // Write the HTML content to the file
+                File.WriteAllText(tempFilePath, htmlContent);
+
+                // Navigate the WebBrowser to the temporary file
+                LogBoxWindow.Navigate(new Uri(tempFilePath));
+
+
+                // Optionally display errors if any
+                //if (!string.IsNullOrEmpty(error))
+                //{
+                //    MessageBox.Show($"Git Status Error:\n{error}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //}
+
+                process.WaitForExit();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Get current date and time
+            DateTime now = DateTime.Now;
+
+            // Format the timestamp string
+            timestamp = $"V{now:yyyyMMddHHmm}";
+
+            // Display the timestamp in the textbox
+            textBox1.Text = timestamp;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog fbd = new CommonOpenFileDialog();
+            
+                fbd.IsFolderPicker = true;
+                fbd.ShowHiddenItems = false;
+
+                if (fbd.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    webBrowser1.Url = new Uri(fbd.FileName);
+                    selectedFolderPath = fbd.FileName;
+                    //GetCurrentBranch();
+                    textBox5.Text = selectedFolderPath;
+                    label7.Text = selectedFolderPath;
+
+                }
+            
+        }
+        void DisplayContent()
+        {
+            // Create simple HTML content
+            string htmlContent = @"
+        <html>
+            <head>
+                <title>Sample Content</title>
+            </head>
+            <body>
+                <h1>Welcome to the WebBrowser Control!</h1>
+                <p>This is some <b>sample content</b> displayed in the WebBrowser control.</p>
+            </body>
+        </html>";
+
+            // Set the HTML content to the WebBrowser control
+            webBrowser1.DocumentText = htmlContent;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Document == null)
+            {
+                //System.Windows.MessageBox.Show("He");
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        protected void GetCurrentBranch()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("git", "branch --show-current");
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            // Set the working directory to the desired repository path
+            startInfo.WorkingDirectory = selectedFolderPath;
+
+            // Create a new process and start it
+            using (Process process = new Process { StartInfo = startInfo })
+            {
+                process.Start();
+
+                // Read the output from the process
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                label8.Text = output;
+
+                process.WaitForExit();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
